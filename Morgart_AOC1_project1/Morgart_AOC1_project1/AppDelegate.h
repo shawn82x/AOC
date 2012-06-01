1a3b4dd97b5fe3f3270f98ef57f1b4c37ed6270f
Shawn Morgart <halcyon@fullsail.edu>
2012-05-31 21:54:27 -0400

    Created Directory
    New. No code modifications yet.

---
+++ Morgart_AOC1_project1/Morgart_AOC1_project1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Morgart_AOC1_project1
+//
+//  Created by Shawn Morgart on 5/31/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
