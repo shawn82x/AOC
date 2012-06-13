45b35023b82fff09d29a0a03968ee5d6e36a30a6
Shawn Morgart <halcyon@fullsail.edu>
2012-06-13 18:02:28 -0400

    Began Project 3

---
+++ Morgart_AOC1_Project3/Morgart_AOC1_Project3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Morgart_AOC1_Project3
+//
+//  Created by Shawn Morgart on 6/13/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
