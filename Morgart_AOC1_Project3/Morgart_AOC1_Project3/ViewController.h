558e6c4deb0228fe163b68e5540676588398a469
Shawn Morgart <halcyon@fullsail.edu>
2012-06-14 16:20:10 -0400

    #1. and #2. done.
    Started on #3.

--- Morgart_AOC1_Project3/Morgart_AOC1_Project3/ViewController.h
+++ Morgart_AOC1_Project3/Morgart_AOC1_Project3/ViewController.h
@@ -9,5 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    
+}
+// 1.
+- (void)myAdd:(NSString*)introString firstInt:(int)firstInt secondInt:(int)secondInt;
+
+// 2.
+- (int)myCompare: (NSString*)introString2 firstInt2:(int)firstInt2 secondInt2:(int)secondInt2;
+
+// 3.
+- (void)myAppend: (NSString*)appString1 appString2:(NSString*)appString2;
 
 @end
--- Morgart_AOC1_Project3/Morgart_AOC1_Project3/ViewController.m
+++ Morgart_AOC1_Project3/Morgart_AOC1_Project3/ViewController.m
@@ -26,6 +26,102 @@
     // Release any retained subviews of the main view.
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    
+    // 1. Call function called "Add".
+    [self myAdd:@"The result for the myAdd function is:" firstInt:2 secondInt:4];
+    
+    // 2. Call function called "Compare".
+    [self myCompare:@"When comparing..." firstInt2:3 secondInt2:4];
+    
+    // 3. Call function called "Append".
+    [self myAppend:@"This is the first half of my sentance..." appString2:@"this is the appended half of my sentance."];
+    
+    [super viewDidAppear:animated];
+    
+    
+    
+    
+}
+
+// 1. Create a function called Add. 
+//    This function will take two NSInteger or int types and 
+//    return the result of an addition between these two.
+
+- (void)myAdd:(NSString*)introString firstInt:(int)firstInt secondInt:(int)secondInt
+    {
+        int addedInts = (firstInt + secondInt);
+        NSLog(@"%@ %d + %d = %d", introString, firstInt, secondInt, addedInts);
+    }
+
+// 2. Create a function called Compare that takes two NSInteger values. 
+//    Return true or false based on whether the values are equal.
+
+- (int)myCompare: (NSString*)introString2 firstInt2:(int)firstInt2 secondInt2:(int)secondInt2
+    {
+        if (firstInt2 == secondInt2)
+            {
+                NSLog(@"%@ %d to %d we find they are Equal", introString2, firstInt2, secondInt2);
+                return TRUE;
+            }
+            else {
+                NSLog(@"%@ %d to %d we find they are NOT equal", introString2, firstInt2, secondInt2);
+                return FALSE;
+            }
+    }
+
+
+// 3. Create a function called Append. 
+//    This function will take two NSStrings and return a new NSString 
+//    containing the appended strings using an NSMutableString and the Append method.
+
+- (void)myAppend: (NSString*)appString1 appString2:(NSString*)appString2
+    {
+        NSMutableString - (void)appendString:(NSString *)aString;
+        NSLog();
+    }
+
+
+// 4. Call the Append function with two NSStrings. 
+//    Capture the result and display a UIAlertView with 
+//    the appended string using displayAlertWithString.
+
+
+// 5. Create a function called DisplayAlertWithString. 
+//    This function will take as a parameter an NSString.
+
+
+// 6. Call the Add function passing in two integer values. 
+//    Capture the return of this function into a variable.
+
+
+// 7. Bundle the returned integer into an NSNumber and then convert it 
+//    to a NSString and pass it to the DisplayAlertWithString function.
+
+
+// 8. Give it some text for the title. 
+//    The message will read, "The number is 00". 
+//    Replace the 00 with the integer passed into the function.
+
+
+// 9. Call the Compare function with two integer values. 
+//    If Compare returns true, display an UIAlertView both with the input values 
+//    and the result using the DisplayAlertWithString function
+
+
+// DisplayAlertWithString Function
+// 1. Take the passed in NSString and display it in the alert view
+// 2. Create an UIAlertView
+
+
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
